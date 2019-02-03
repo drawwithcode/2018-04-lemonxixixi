3582041f0c37531b33eebd59f5ed74cadc5cd455
lemonxixixi <43404481+lemonxixixi@users.noreply.github.com>
2019-02-03 17:46:49 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
-function preload(){
-  // put preload code here
+var bubbles = [];
+function setup(){
+  createCanvas(600,400);
+  for(var i =0; i < 200; i++){
+    bubbles[i] = {
+      x:0,
+      y:0,
+     display: function(){
+        stroke(255);
+        noFill();
+        ellipse(this.x,this.y,25,25);
+      },
+      move: function(){
+        this.x = this.x + random(-5,5);
+        this.y = this.y + random(-5,5);
+      }
+    }
+  }
 }
-
-function setup() {
-  // put setup code here
-}
-
-function draw() {
-  // put drawing code here
+function draw(){
+  translate(mouseX,mouseY);
+  background(0);
+  for(var i = 0; i < bubbles.length; i++){
+    bubbles[i].move();
+    bubbles[i].display();
+  }
 }
